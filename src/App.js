5a2f3bef942bdad1749a82d08a85d08653e42511
Mikita N <nuktinov@gmail.com>
2023-07-25 00:18:16 +0200

    MM-33: Fix UI bugs

--- src/App.js
+++ src/App.js
@@ -5,20 +5,12 @@ import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import store from './store/store';
 import authService from './services/authService'
-import routes from './constants/route-constants';
 
 import AuthProvider from './components/Auth/authProvider'
-import PrivateRoute from './components/Auth/protectedRoute'
-import SigninOidc from './components/Auth/signin-oidc'
-import SignoutOidc from './components/Auth/signout-oidc'
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import StartPage from './components/StartPage/StartPage';
-import AllEventsPage from './components/AllEventsPage/AllEventsPage';
-import OwnEventsPage from './components/OwnEventsPage/OwnEventsPage';
-import CalendarPage from './components/CalendarPage/CalendarPage';
-import SpecificEventPage from './components/SpecificEventPage/SpecificEventPage';
 import LoadingIndicator from './components/LoadingIndicator/LoadingIndicator';
+import Main from 'components/Main/Main'
 
 export default function App() {
 
@@ -32,17 +24,7 @@ export default function App() {
         <AuthProvider userManager={authService.getUserManager()} store={store}>
           <Router>
             <Header />
-            <main>
-              <Switch>
-                <Route path={routes.signin} component={SigninOidc} />
-                <Route path={routes.signout} component={SignoutOidc} />
-                <Route exact path={routes.root} component={StartPage} />
-                <PrivateRoute path={`${routes.events}/:id`} component={SpecificEventPage} />
-                <PrivateRoute path={routes.events} component={AllEventsPage} />
-                <PrivateRoute path={routes.ownEvents} component={OwnEventsPage} />
-                <PrivateRoute path={routes.calendar} component={CalendarPage} />
-              </Switch>
-            </main>
+            <Main />
             <Footer />
             <LoadingIndicator />
           </Router>
---
+++ src/components/Main/Main.js
@@ -0,0 +1,28 @@
+import routes from 'constants/route-constants'
+import { Route, Switch } from 'react-router-dom'
+
+import PrivateRoute from 'components/Auth/protectedRoute'
+import SigninOidc from 'components/Auth/signin-oidc'
+import SignoutOidc from 'components/Auth/signout-oidc'
+import StartPage from 'components/StartPage/StartPage';
+import AllEventsPage from 'components/AllEventsPage/AllEventsPage';
+import OwnEventsPage from 'components/OwnEventsPage/OwnEventsPage';
+import CalendarPage from 'components/CalendarPage/CalendarPage';
+import SpecificEventPage from 'components/SpecificEventPage/SpecificEventPage';
+
+export default function Main () {
+
+    return (
+        <main>
+            <Switch>
+                <Route path={routes.signin} component={SigninOidc} />
+                <Route path={routes.signout} component={SignoutOidc} />
+                <Route exact path={routes.root} component={StartPage} />
+                <PrivateRoute path={`${routes.events}/:id`} component={SpecificEventPage} />
+                <PrivateRoute path={routes.events} component={AllEventsPage} />
+                <PrivateRoute path={routes.ownEvents} component={OwnEventsPage} />
+                <PrivateRoute path={routes.calendar} component={CalendarPage} />
+            </Switch>
+        </main>
+    )
+}
